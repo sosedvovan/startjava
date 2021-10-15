ac59d2dc36f4718811f14818bd5189efa1d6cf01
Vladimir <drugst@yandex.ru>
2021-10-15 20:58:07 +0300

    implement proverka

--- lesson_2/src/CalculatorTest.java
+++ lesson_2/src/CalculatorTest.java
@@ -13,18 +13,34 @@ public class CalculatorTest{
             Scanner scan = new Scanner(System.in);
             Calculator calc = new Calculator();
 
-            System.out.print("Input first number: ");
-            int firstNumber = scan.nextInt();
-            calc.setFirstNumber(firstNumber);
+            System.out.print("Input first number (int): ");
+            if (scan.hasNextInt()){// возвращает истинну если с потока ввода можно считать целое число
+                int firstNumber = scan.nextInt();// считывает целое число с потока ввода и сохраняем в переменную
+                calc.setFirstNumber(firstNumber);
+            }else
+                {System.out.println("Error: not int!");
+                    continue;//прервет данный виток цикла while и запустит новый виток сначала
+                }
+
 
             System.out.print("Input arithmetic operation: ");
-            char calcOperation = scan.next().charAt(0);
+            char calcOperation = scan.next().charAt(0);//из того, что вернет next() - charAt(0) возьмет первый символ
             calc.setCalcOperation(calcOperation);
 
 
-            System.out.print("Input second number: ");
-            int secondNumber = scan.nextInt();
-            calc.setSecondNumber(secondNumber);
+
+            System.out.print("Input second number (int): ");
+                if(scan.hasNextInt()) {
+                    int secondNumber = scan.nextInt();
+                    calc.setSecondNumber(secondNumber);
+                }else {
+                    System.out.println("Error: not int!");
+                    continue;//прервет данный виток цикла while и запустит новый виток сначала
+                }
+
+
+
+
 
             System.out.println(calc);
 
