20a90fed3a783b4ea1c4c77b707bf3046dcfe7b2
Vladimir <drugst@yandex.ru>
2021-10-16 12:58:40 +0300

    idea start 2

--- lesson_1/Unicode.java
+++
@@ -1,8 +0,0 @@
-public class Unicode {
-	public static void main (String[] args) {
-        for (int i = 33; i <= 126; i++){
-            char ch = (char)i;
-            System.out.println(ch);
-        }
-    }
-}
