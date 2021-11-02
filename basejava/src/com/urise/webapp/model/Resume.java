8923b1fd13334edbbd3279bce557b079f108fae0
Vladimir <drugst@yandex.ru>
2021-11-02 13:41:36 +0300

    lesson_5_Collection_3

--- basejava/src/com/urise/webapp/model/Resume.java
+++ basejava/src/com/urise/webapp/model/Resume.java
@@ -1,60 +1,89 @@
 package com.urise.webapp.model;
 
+import java.util.Objects;
 import java.util.UUID;
 
-//implements Comparable<Resume> говорит о том что объекты этого класса можно сравнивать-
-//чтобы работал метод Arrays.binarySearch()-поиск индекса по значению методом бинарного деления на 2
-//реализуем метод compareTo() этого интерфейса и подправим его вручную. см ниже.
-public class Resume implements Comparable<Resume>{
+/** <p> implements Comparable<Resume> говорит о том что объекты этого класса можно сравнивать-
+* чтобы работал метод Arrays.binarySearch()-поиск индекса по значению методом бинарного деления на 2
+* в классе SortedArrayStorage.
+* реализуем - (@Override) метод compareTo() этого интерфейса(Comparable) и подправим его вручную. см ниже.
+
+* убрали implements Comparable<Resume> кот нужен был для метода getIndex()с бинарным поиском в классе SortedArrayStorage
+* в классе SortedArrayStorage вместо Comparable  реализуем Comparator с пом лямбды.</p>
+ */
+
+public class Resume /*implements Comparable<Resume>*/{
 
     //Unique identifier
-    private final String uuid;//сделали поле final -> обязат. нужено инициализировать
-                                // можно ициализировать с пом конструктора, а сеттер надо удалить?
+    private final String uuid;
+    /** <p>
+     *сделали поле final -> обязат. нужено инициализировать
+     * можно ициализировать с пом конструктора, а сеттер надо удалить тк final
+     </p>*/
+
     //private  String fullName;
 
-    public Resume(){//это конструктор без параметров с автогенерацией uuid
-        this(UUID.randomUUID().toString());//с пом. this он подает автосгенерированную строку
-                                            //в конструктор с параметрами(который ниже)
-                                            //но мы его не исспользуем пока-он бледный
-    }
 
+    public Resume(){//
+        this(UUID.randomUUID().toString());
+    }
+    /** <p>
+     * выше конструктор без параметров с автогенерацией uuid
+     * с пом. this он подает автосгенерированную строку
+     * в конструктор с параметрами(который ниже)
+     * </p>*/
     public Resume(String uuid) {
         this.uuid = uuid;
     }
 
+    //геттер:
     public String getUuid() {
         return uuid;
     }
 
-    //сеттер удалили тк поле uuid сделали final и теперь можно только через конструктор с ним работать
-    //public void setUuid(String uuid) {
-    //    this.uuid = uuid;
-   // }
+    /** <p>
+     *
+     * сеттер удалили тк поле uuid сделали final и теперь можно только через конструктор с ним работать
+     * public void setUuid(String uuid) {
+     * this.uuid = uuid;
+     * }
+     *
+     </p>*/
 
+    //переопределили equals():
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;//если в аргументах подали тот же обект
-        if (o == null || getClass() != o.getClass()) return false;//если подали null вместо объекта
-                                                                    // или объект другого класса
+
+        if (o == null || getClass() != o.getClass()) return false;
+        //если подали null вместо объекта, или объект другого класса
 
         Resume resume = (Resume) o;//приводим Object o к нашему классу
 
-        return uuid != null ? uuid.equals(resume.uuid) : resume.uuid == null;//проверка поля объекта на null
-                                                                              // и сравнение поля обекта
+        return Objects.equals(uuid, resume.uuid);//проверка поля объекта на null
+        //проверка поля объекта на null  и сравнение поля обекта
     }
 
+    //переопределили hashCode()
     @Override
     public int hashCode() {
         return uuid != null ? uuid.hashCode() : 0;
     }
 
+    //переопределили toString()
     @Override
     public String toString() {
         return uuid;
     }
 
-    @Override
-    public int compareTo(Resume o) { //реализовали тк этот класс implements Comparable<>
-        return uuid.compareTo(o.uuid);//это дописали руками наподобии equals
-    }
+    /** <p>
+     * реализовали этот метод когда этот класс implements Comparable<>
+     * но потом Comparable заменили на Comparator с лямбдой в классе SortedArrayStorage
+     *
+     * @Override
+     * public int compareTo(Resume o) {
+     *  return uuid.compareTo(o.uuid);//это дописали руками наподобии equals
+     * }
+     *
+    </p>*/
 }
--- basejava/src/com/urise/webapp/storage/SortedArrayStorage.java
+++ basejava/src/com/urise/webapp/storage/SortedArrayStorage.java
@@ -5,12 +5,15 @@ package com.urise.webapp.storage;
 import com.urise.webapp.model.Resume;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 //на основе сортированного массива, чтобы большое О стало не линейным(перебор всего массива)
 //а большое О стало от логарифма с основанием 2(бинарный поиск в массиве методом деления на 2)
 //этот класс implements Storage тк его родительский AbstractArrayStorage implements Storage
 
 public class SortedArrayStorage extends AbstractArrayStorage {
+
+    private static final Comparator<Resume> RESUME_COMPARATOR = ( o1, o2 ) -> o1.getUuid().compareTo(o2.getUuid());
 
 
     //ЭТОТ МЕТОД- ЧАСТЬ ЛОГИКИ РЕАЛИЗУЕМОЙ В РОДИТЕЛЬСКОМ КЛАССЕ (ШАБЛОННЫЙ ПАТТЕРН)
@@ -26,12 +29,79 @@ public class SortedArrayStorage extends AbstractArrayStorage {
         //сеттер удалим тк стали использовать конструктор
         //searchKey.setUuid(uuid);//новому объекту присваеваем искомое значение поля из аргументов метода
 
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
         //где Arrays.binarySearch() уже имеющийся в java метод класса  Arrays для нахождения индекса по значению
         //методом деления на 2. на вход надо подать наш массив, интервал поиска(0-size) и объект содержащий
         //искомое значение-uuid
         //причем storage должен быть отсортирован и по uuid должен быть переопределен equals(в Resume)
         // чтобы сравнивать класс Resume должен implements Comparable<Resume>
+        //если убрать implements Comparable<Resume> с Resume- тест завалится с
+        //эксепшеном- что я не могу Resume преобразовать в Comparable - ClassCastException
+        //Comparable- значит что объект может сравниться сам с собой
+        //Comparator-отдельный в стороночке класс, кот может сравнивать 2-а объекта
+        //и методу Arrays.binarySearch() нужно чтобы Resume implements Comparable<Resume>
+        //те массив у которого элементы Comparable
+        //или сторонний класс Comparator, кот позволяет сравнивать 2-а обекта
+        /**
+         * если убрать implements Comparable<Resume> с Resume:
+         * тогда реализуем внутренний статический класс -
+         * (без скрытой ссылки на внешний класс) после метода getIndex():
+         *
+         * private static class ResumeComparator implements Comparator<Resume>{
+         * @Override
+         * public int compare(Resume o1, Resume o2){
+         * return o1.getUuid().compareTo(o2.getUuid());
+         *  }
+         * }
+         *
+         * а в теле метода getIndex() в аргументах метода Arrays.binarySearch()
+         * добавим объект этого внутреннего класса ResumeComparator :
+         * return Arrays.binarySearch(storage, 0, size, searchKey, new ResumeComparator);
+         *
+         * чтобы каждый раз при вызове метода getIndex() не создавать new ResumeComparator
+         * вынесем его в константы в полях этого класса:
+         * private static final ResumeComparator RESUME_COMPARATOR = new ResumeComparator;
+         * и заменим в теле метода getIndex():
+         * return Arrays.binarySearch(storage, 0, size, searchKey, RESUME_COMPARATOR);
+         *
+         * получается, что мы создаем вложенный класс чтобы создать только один объект : new ResumeComparator
+         * мы можем сократить код: объединив создание константы RESUME_COMPARATOR с анонимным классом:
+         *
+         * private static final Comparator<Resume> RESUME_COMPARATOR = new Comparator<Resume>(){
+         * @Override
+         * public int compare(Resume o1, Resume o2){
+         * return o1.getUuid().compareTo(o2.getUuid());
+         * }
+         * }
+         * тк мы убрали класс ResumeComparator, соответственно при объявлении RESUME_COMPARATOR
+         * тип ResumeComparator изменили на Comparator<Resume>.
+         *
+         * то образом Ява создаст класс без названия(анонимный) и один объект этого класса
+         * с сылкой RESUME_COMPARATOR. при обращении по этой ссылки запускается метод
+         * этого анонимного класса, возвращающий индекс элемента по значению его поля,
+         * или если элемент не найден возвращается отрицательное число
+         *
+         * пойдем дальше:
+         * new Comparator<Resume>() будет серенького цвета-> Идеа хочет оптимизировать код:
+         * Alt+Enter -> преобразует в лямда выражение:
+         *
+         * private static final Comparator<Resume> RESUME_COMPARATOR =
+         * ( o1, o2 ) -> o1.getUuid().compareTo(o2.getUuid());
+         *
+         * те мы принимаем ( o1, o2 ) и возвращаем o1.getUuid().compareTo(o2.getUuid())
+         *
+         * лямда выражение можно вернуть к обычному виду- стать курсором на ->
+         * и выбрать Replace lambda with anonymous class
+         *
+         * те если есть интерфейс с единственным методом, который мы можем реализовать,
+         * то мы можем заменить его на лямбду
+         *
+         * лямбда реализована внутри JVM и мы не можем увидить ее реализацию
+         * лямбда потребляет меньше ресурсов, чем анонимный класс
+         *
+         * если Идеа подчеркнула лямбду желтым -> Alt+Enter и возможна дальнейшая оптимизация
+         */
+
     }
 
     //------------------------------------------------------------------------------------------------------------------
